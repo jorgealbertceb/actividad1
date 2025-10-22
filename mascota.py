7a65c9e6aa010058a0ea70a4203d94b099dd89d3
jorgealbertmora <jorge.albert.ceb@immune.institute>
2025-10-22 18:38:43 +0200

    Modificado fichero mascota

--- mascota.py
+++ mascota.py
@@ -1,4 +1,4 @@
 nombre_mascota = str(input("Introduzca el nombre de su mascota: "))
 mascota = str(input("¿Qué es tu mascota?: "))
 
-print(f"Tu {mascota} se llama {nombre_mascota})
+print(f"Tu {mascota} se llama {nombre_mascota}")
