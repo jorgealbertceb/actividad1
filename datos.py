0bc34f8d638a38b1db365926cb0104c7cc87690a
jorgealbertmora <jorge.albert.ceb@immune.institute>
2025-10-22 18:34:26 +0200

    Desarrollado codigo principal del programa

---
+++ datos.py
@@ -0,0 +1,5 @@
+nombre = str(input("Ingrese su nombre: "))
+edad = int(input("Ingrese su edad: "))
+hobbie = str(input("Ingrese su hobbie favorito: "))
+
+print(f"Hola, mi nombre es {nombre}, tengo {edad} años y me gusta {hobbie}.")
