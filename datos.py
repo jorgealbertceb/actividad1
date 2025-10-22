a9e28bd6ef365db9d9cb40edbcae3752b6a9362f
Jorge <122154386+jorgealbertceb@users.noreply.github.com>
2025-10-22 18:40:43 +0200

    ggg

--- datos.py
+++ datos.py
@@ -3,3 +3,5 @@ edad = int(input("Ingrese su edad: "))
 hobbie = str(input("Ingrese su hobbie favorito: "))
 
 print(f"Hola, mi nombre es {nombre}, tengo {edad} años y me gusta {hobbie}.")
+
+print("HOLAAAAAAA")
