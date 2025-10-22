f5af43eb04cdbc9ce95a30f216f438c58d2e47a3
jorgealbertmora <jorge.albert.ceb@immune.institute>
2025-10-22 19:00:44 +0200

    nuevo fichero matematico

---
+++ mates.py
@@ -0,0 +1,7 @@
+numero = int(input("Ingrese un número entero: "))
+
+if numero % 2 == 0:
+    print(f"El número {numero} es múltiplo de 2.")
+
+else:
+    print(f"El número {numero} no es múltiplo de 2.")
